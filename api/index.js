3a828f6f3f2b507e9bd1242720ce88d711f0d5b9
ns <clidertutayarivera@gmail.com>
2024-11-17 09:05:53 -0500

    re

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // import app from "../src/index.js"
 
-// // Exporta la app como manejador de funciones serverless
-// export default(req, res) =>{
-//     console.log('Headers:', req.headers);
-//     console.log('Body:', req.body);
-//     app(req,res)
-// }
+// Exporta la app como manejador de funciones serverless
+export default(req, res) =>{
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    app(req,res)
+}
--- src/index.js
+++ src/index.js
@@ -45,8 +45,8 @@ app.use((err, req, res, next) => {
 })
 
 /*Pausar inicializaicion de puerto para SERVERLESS*/
-const PORT = process.env.PORT || 2000;
-app.listen(PORT, () => { console.log(`Servidor corriendo en  el puerto ${PORT}`) })
+// const PORT = process.env.PORT || 2000;
+// app.listen(PORT, () => { console.log(`Servidor corriendo en  el puerto ${PORT}`) })
 
 //Exportar configuracion
-//export default app;
+export default app;
